2f558d04a279a326a94fbaec4072dc7df32972bf
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-03-31 19:52:08 +0200

    basic next.js template created

--- app/api/products.js
+++
@@ -1,7 +0,0 @@
-import { initMongoose } from "@/lib/mongoose";
-import Product from "@/models/product";
-
-export default async function handle(req, res) {
-    await initMongoose();
-    res.json( await Product.find().exec());
-}
