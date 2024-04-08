2d4691617fdca7377c80ba9f8733bdce2777c3c9
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-09 00:17:12 +0200

    page actualiced and product card added

---
+++ app/components/products/ProductCard.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+const ProductCard = () => {
+    return( 
+        <div className="col-span-1">
+            <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+        </div>
+    );
+};
+ 
+export default ProductCard;
