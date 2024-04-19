cd82ff5ea41497cb4ea14dbba4dc774d7e60c9f5
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-20 00:18:29 +0200

    product card modified

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -2,15 +2,22 @@
 
 import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
 
 interface ProductCardProps{
     data: any
 }
 
+
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
-    return( 
-        <div className="col-span-1 
+
+     const productRating =
+       data.reviews.reduce((acc:number, item:any) => item.rating + acc, 0) / data.reviews.length;
+
+    return (
+      <div
+        className="col-span-1 
         cursor-pointer 
         border-[1.2px]
         border-slate-200
@@ -21,30 +28,33 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
         hover:scale-105
         text-center
         text-sm
-        ">
-            <div className="
+        "
+      >
+        <div
+          className="
             flex
             flex-col
             items-center
             w-full
             gap-1
-            ">
-                <div className="aspect-square overflow-hidden relative w-full">
-                    <Image 
-                    fill
-                    src={data.images[0].image}
-                    alt={data.name}
-                    className="w-full h-full object-contain"
-                    />
-                </div>
-                <div className="mt-4">
-                    {truncateText(data.name)}
-                </div>
-                <div></div>
-                <div>{data.reviews.length} reviews</div>
-                <div>{formatPrice(data.price)}</div>
-            </div>
+            "
+        >
+          <div className="aspect-square overflow-hidden relative w-full">
+            <Image
+              fill
+              src={data.images[0].image}
+              alt={data.name}
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <div className="mt-4">{truncateText(data.name)}</div>
+          <div>
+            <Rating value={productRating} readOnly />
+          </div>
+          <div>{data.reviews.length} reviews</div>
+          <div className="font-semibold">{formatPrice(data.price)}</div>
         </div>
+      </div>
     );
 };
  
