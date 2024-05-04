9d884fe86ff9c42b1dbb7c6dbd08ed5e324737c0
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-04 13:37:17 +0200

    Product image component added and image selector working

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { Rating } from "@mui/material";
 import SetColor from '@/app/components/products/SetColor';
 import SetQuantity from '@/app/components/products/SetQuantity';
 import Button from '@/app/components/Button';
+import ProductImage from '@/app/components/products/ProductImage';
 
 interface ProductDetailsProps{
     product: any
@@ -89,7 +90,11 @@ const handleQtyDecrease = useCallback(() => {
 
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div>Images</div>
+        <ProductImage 
+          cartProduct={cartProduct} 
+          product={product} 
+          handleColorSelect={handleColorSelect}
+        />
         <div className="flex flex-col gap-1 text-slate-500 text-sm">
           <h2 className="text-3xl font-medium text-slate-700">
             {product.name}
