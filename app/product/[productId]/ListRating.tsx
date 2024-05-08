8f28aec12fddcb18868aee9ad45b085b384930e5
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-08 22:43:16 +0200

    List Rating prop added

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import moment from "moment";
+import { Rating } from "@mui/material";
+
 
 interface ListRatingProps {
     product: any;
@@ -13,7 +16,16 @@ const ListRating: React.FC<ListRatingProps> = ({product}) => {
         <div className="text-sm mt-2">
             {product.reviews && product.reviews.map((review: any) => {
                 return <div key={review.id} className="max-w-300px">
-
+                    <div className="flex gap-2 items-center">
+                        <div>Avatar</div>
+                        <div className="font-semibold">{review?.user.name}</div>
+                        <div className="font-ligth">{moment(review.createdDate).fromNow()}</div>
+                    </div>
+                    <div className="mt-2">
+                        <Rating value = {review.rating} readOnly/>
+                        <div className="ml-2">{review.comment}</div>
+                        <hr className="mt-4 mb-4" />
+                    </div>
                 </div>
             })}
         </div>
