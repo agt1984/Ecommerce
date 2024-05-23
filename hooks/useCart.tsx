33d7b72e3ea3fc389b8778bc7539d003b14f6d54
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-23 22:22:37 +0200

    CartContextProvide added

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,8 +1,11 @@
 //Este es el use context para el carrito, es un hook
-import { createContext, useContext, useState } from "react";
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { createContext, useCallback, useContext, useState } from "react";
 
 type CartContextType = {
     cartTotalQty: number;
+    cartProducts: CartProductType[] | null;
+    handleAddProductToCart: (product: CartProductType) => void
 };
 
 export const CartContext = 
@@ -14,9 +17,26 @@ interface Props {
 
 export const CartContextProvider = (props: Props) => {
     const [cartTotalQty, setCartTotalQty] = useState(0);
+    const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null)
+
+    const handleAddProductToCart = useCallback((product: CartProductType)=>{
+        setCartProducts((prev) => {
+            let updatedCart;
+
+            if(prev){
+                updatedCart = [...prev, product]
+            }else{
+                updatedCart = [product]
+            }
+
+            return updatedCart;
+        })
+    }, [])
 
     const value = {
       cartTotalQty,
+      cartProducts,
+      handleAddProductToCart,
     };
 
     return <CartContext.Provider value={value} {...props} />;
